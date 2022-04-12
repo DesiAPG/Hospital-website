4346d8d6d1d0315f6902fbfc159f8813a489270b
DesiAPG <desireapgames@gmail.com>
2022-04-12 12:16:13 -0500

    Added the header section

--- public/main.js
+++ public/main.js
@@ -2,7 +2,6 @@ const navItems = document.querySelector("#nav__items");
 const openNavBtn = document.querySelector("#open__nav-btn");
 const closeNavBtn = document.querySelector("#close__nav-btn");
 
-
 openNavBtn.addEventListener("click", () => {
   navItems.style.display = "flex";
   openNavBtn.style.display = "none";
